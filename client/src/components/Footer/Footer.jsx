1cf655d46d0e9bf57235c7509f68d6e1c9553076
DanielVLV <odindlrus@gmail.com>
2023-04-12 12:45:13 +0400

    footor homePage and swiper

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Footer.css';
 
 function Footer() {
   return (
-    <div className="footerAllDiv">
-      <div>footer</div>
-      <Link to="/">о нас</Link>
+    <div className="footerAllDiv" style={{ }}>
+      <Link className="text" to="/">О компании</Link>
+      <Link className="text" to="/contacts">Контакты</Link>
     </div>
 
   );
