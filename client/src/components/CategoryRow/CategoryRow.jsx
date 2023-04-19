4d7c973471ede1814fe9b1184fe90b3a3c373bac
Pavel <goncharov_p@bk.ru>
2023-04-19 12:33:04 +0300

    favsButtons

--- client/src/components/CategoryRow/CategoryRow.jsx
+++ client/src/components/CategoryRow/CategoryRow.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable object-curly-newline */
 /* eslint-disable import/no-unresolved */
 /* eslint-disable quotes */
 /* eslint-disable react/prop-types */
@@ -9,13 +10,16 @@ import "swiper/css/navigation";
 import { Pagination, Navigation } from "swiper";
 import { Link } from "react-router-dom";
 import ProductElement from "../ProductElement/ProductElement";
-import styles from './categoryRow.css';
+import styles from "./categoryRow.css";
 
-
-function CategoryRow({ el, setOpen }) {
+function CategoryRow({ el, setOpen, setFavs, allFavs, loading }) {
   return (
     <div className={styles.columnSwiperProducts}>
-      {el.Products.length && <Link to={`/categories/${el.id}`}><h2>{el.categoryName}</h2></Link>}
+      {el.Products.length && (
+        <Link to={`/categories/${el.id}`}>
+          <h2>{el.categoryName}</h2>
+        </Link>
+      )}
       <Swiper
         slidesPerView={3}
         spaceBetween={60}
@@ -27,10 +31,16 @@ function CategoryRow({ el, setOpen }) {
         modules={[Pagination, Navigation]}
         className={styles.mySwiper}
       >
-
         {el.Products.map((product) => (
           <SwiperSlide>
-            <ProductElement key={product.id} product={product} setOpen={setOpen} />
+            <ProductElement
+              key={product.id}
+              product={product}
+              setOpen={setOpen}
+              setFavs={setFavs}
+              allFavs={allFavs}
+              loading={loading}
+            />
           </SwiperSlide>
         ))}
       </Swiper>
